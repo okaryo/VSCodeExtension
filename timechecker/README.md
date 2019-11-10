bf73479c60ece048d38d9ca2bd55a727245f9c1a
okaryo <okaryotr@gmail.com>
2019-11-10 20:03:01 +0900

    first commit

---
+++ timechecker/README.md
@@ -0,0 +1,19 @@
+# timechecker README
+
+## Features
+- Current time is displayed at the status bar.
+- Total time from opening editor is displayed together.
+
+## Requirements
+
+## Extension Settings
+
+## Known Issues
+
+## Release Notes
+
+### 0.0.1
+
+Initial release of ...
+
+-----------------------------------------------------------------------------------------------------------
---
+++ timechecker/src/extension.ts
@@ -0,0 +1,31 @@
+import * as vscode from 'vscode';
+import * as dayjs from 'dayjs';
+
+let myStatusBarItem: vscode.StatusBarItem;
+
+export function activate(context: vscode.ExtensionContext) {
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 10000);
+	let startTime = dayjs();
+	
+	setInterval(() => {
+		let totalSecond = dayjs().diff(startTime, 'second');
+		myStatusBarItem.text = `${dayjs().format('YYYY-MM-DD HH:mm:ss')}(total: ${secondFormater(totalSecond)})`;
+		myStatusBarItem.show();
+	}, 1000);
+}
+
+function secondFormater(second: number) {
+	let hours: number;
+	let minutes : number;
+  if (second < 60) {
+		return '0m';
+	} else if (second < 60 * 60) {
+		minutes = second / 60;
+		return `${Math.floor(minutes)}m`;
+	} else {
+		hours   = second / (60 * 60);
+		second  %= 60 * 60;
+		minutes = second / 60;
+		return `${Math.floor(hours)}h${Math.floor(minutes)}m`;
+	}
+}
